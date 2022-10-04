491443db0cacf334780dc41dc66edfd078da3fd8
Mahdi <mxhdi.sh@gmail.com>
2022-10-04 09:47:36 +0330

    Making the Mobile Navigation Work

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
-yearEl.textContent = currentYear; 
+yearEl.textContent = currentYear;
 
+/////////////////////////////////////////////////////////////
+// make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
